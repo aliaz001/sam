77f2892bf9639998a5f4679bf414f93ab3b8e7b6
aliaz001 <alihosene00093@gmail.com>
2017-07-08 21:43:22 +0430

    ali
    ali

---
+++ data/config.lua
@@ -0,0 +1,20 @@
+do local _ = {
+  admins = {},
+  disabled_channels = {},
+  enabled_plugins = {
+    "banhammer",
+    "groupmanager",
+    "msg-checks",
+    "plugins",
+    "tools"
+  },
+  info_text = "fire v2\n\n\n》\n\n》Admins :\n》@aliaz003 《\n\9\9\n\n",
+  moderation = {
+    data = "./data/moderation.json"
+  },
+  sudo_users = {
+    104280701
+  }
+}
+return _
+end
